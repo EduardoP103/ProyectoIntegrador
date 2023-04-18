f3d03b1a414c5d353ea00727efa2aed3c3db03b3
Guillermo Eduardo Narvaez Muggi <guillermo.narvaez@seidor.com>
2023-04-18 12:55:20 -0500

    Se crea el proyecto base integrador

---
+++ uimodule/webapp/test/integration/arrangements/Startup.js
@@ -0,0 +1,21 @@
+sap.ui.define(
+    ["sap/ui/test/Opa5"],
+    /**
+     * @param {typeof sap.ui.test.Opa5} Opa5
+     */
+    function (Opa5) {
+        "use strict";
+
+        return Opa5.extend("com.pe.proyectoIntegrador.test.integration.arrangements.Startup", {
+            iStartMyApp: function () {
+                this.iStartMyUIComponent({
+                    componentConfig: {
+                        name: "com.pe.proyectoIntegrador",
+                        async: true,
+                        manifest: true
+                    }
+                });
+            }
+        });
+    }
+);
