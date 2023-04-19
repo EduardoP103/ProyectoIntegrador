a37a4d08c16a48ac2c2561d4ea32d9f3c9e2a853
Guillermo Eduardo Narvaez Muggi <guillermo.narvaez@seidor.com>
2023-04-18 19:05:29 -0500

    Se adjuntan cambios

--- uimodule/webapp/model/models.js
+++ uimodule/webapp/model/models.js
@@ -17,6 +17,188 @@ sap.ui.define(
                 oModel.setDefaultBindingMode("OneWay");
                 return oModel;
             },
+            localModel : function(){
+                const oParam = {
+                    nombre: "",
+                    direccion: "",
+                    selectKeyPais: "0",
+                    selectKeyDistrito : "0",
+                    listaPokemones : [],
+                    // listaTabla : [],
+                    listaTabla : [
+                        {
+                            "product": "Carro",
+                            "supplier": "BMW",
+                            "dimension": "4 x 4",
+                            "weight": "weight1",
+                            "price": "10.0"
+                          },
+                          {
+                            "product": "Mouse",
+                            "supplier": "supplier12",
+                            "dimension": "dimension2",
+                            "weight": "weight2",
+                            "price": "12.0"
+                          },
+                          {
+                            "product": "Aceituna",
+                            "supplier": "supplier13",
+                            "dimension": "dimension3",
+                            "weight": "weight3",
+                            "price": "13.0"
+                          },
+                          {
+                            "product": "Penicilina",
+                            "supplier": "supplier14",
+                            "dimension": "dimension4",
+                            "weight": "weight4",
+                            "price": "17.0"
+                          },
+                          {
+                            "product": "Mouse",
+                            "supplier": "supplier12",
+                            "dimension": "dimension2",
+                            "weight": "weight2",
+                            "price": "12.0"
+                          },
+                          {
+                            "product": "Aceituna",
+                            "supplier": "supplier13",
+                            "dimension": "dimension3",
+                            "weight": "weight3",
+                            "price": "13.0"
+                          },
+                          {
+                            "product": "Penicilina",
+                            "supplier": "supplier14",
+                            "dimension": "dimension4",
+                            "weight": "weight4",
+                            "price": "17.0"
+                          },
+                          {
+                            "product": "Mouse",
+                            "supplier": "supplier12",
+                            "dimension": "dimension2",
+                            "weight": "weight2",
+                            "price": "12.0"
+                          },
+                          {
+                            "product": "Aceituna",
+                            "supplier": "supplier13",
+                            "dimension": "dimension3",
+                            "weight": "weight3",
+                            "price": "13.0"
+                          },
+                          {
+                            "product": "Penicilina",
+                            "supplier": "supplier14",
+                            "dimension": "dimension4",
+                            "weight": "weight4",
+                            "price": "17.0"
+                          },
+                          {
+                            "product": "Mouse",
+                            "supplier": "supplier12",
+                            "dimension": "dimension2",
+                            "weight": "weight2",
+                            "price": "12.0"
+                          },
+                          {
+                            "product": "Aceituna",
+                            "supplier": "supplier13",
+                            "dimension": "dimension3",
+                            "weight": "weight3",
+                            "price": "13.0"
+                          },
+                          {
+                            "product": "Penicilina",
+                            "supplier": "supplier14",
+                            "dimension": "dimension4",
+                            "weight": "weight4",
+                            "price": "17.0"
+                          },
+                          {
+                            "product": "Mouse",
+                            "supplier": "supplier12",
+                            "dimension": "dimension2",
+                            "weight": "weight2",
+                            "price": "12.0"
+                          },
+                          {
+                            "product": "Aceituna",
+                            "supplier": "supplier13",
+                            "dimension": "dimension3",
+                            "weight": "weight3",
+                            "price": "13.0"
+                          },
+                          {
+                            "product": "Penicilina",
+                            "supplier": "supplier14",
+                            "dimension": "dimension4",
+                            "weight": "weight4",
+                            "price": "17.0"
+                          },
+                          {
+                            "product": "Mouse",
+                            "supplier": "supplier12",
+                            "dimension": "dimension2",
+                            "weight": "weight2",
+                            "price": "12.0"
+                          },
+                          {
+                            "product": "Aceituna",
+                            "supplier": "supplier13",
+                            "dimension": "dimension3",
+                            "weight": "weight3",
+                            "price": "13.0"
+                          },
+                          {
+                            "product": "Penicilina",
+                            "supplier": "supplier14",
+                            "dimension": "dimension4",
+                            "weight": "weight4",
+                            "price": "17.0"
+                          },
+                          {
+                            "product": "Mouse",
+                            "supplier": "supplier12",
+                            "dimension": "dimension2",
+                            "weight": "weight2",
+                            "price": "12.0"
+                          },
+                          {
+                            "product": "Aceituna",
+                            "supplier": "supplier13",
+                            "dimension": "dimension3",
+                            "weight": "weight3",
+                            "price": "13.0"
+                          },
+                          {
+                            "product": "Penicilina",
+                            "supplier": "supplier14",
+                            "dimension": "dimension4",
+                            "weight": "weight4",
+                            "price": "17.0"
+                          }
+                    ],
+
+                    selectPokemon : {
+                        visible : false,
+                        key : "0",
+                        url : ""
+                    },
+                    addReg : {
+                        "product": "",
+                        "supplier": "",
+                        "dimension": "",
+                        "weight": "",
+                        "price": ""
+                      },
+                      
+                }
+                const oModel = new JSONModel(oParam);
+                return oModel;
+            }
         };
     }
 );
