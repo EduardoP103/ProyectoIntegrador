d111574194d1ceabdb3fec8f6fe98f5a62eec257
Eyner Mesones <eyner.mesones@seidor.com>
2023-04-21 12:29:24 -0500

    Avance de la vista. Faltan PopOvers

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -2,13 +2,16 @@ sap.ui.define(
     ["./BaseController",
     "sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
+    "sap/f/library",
 	"sap/ui/core/Fragment",
-    'sap/ui/core/util/Export', 
-    'sap/ui/core/util/ExportTypeCSV'],
+    "sap/ui/core/util/Export", 
+    "sap/ui/core/util/ExportTypeCSV",
+    "sap/m/MessageBox",
+	"sap/ui/core/Fragment"],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel, Fragment) {
+    function (Controller, JSONModel, Fragment, library, MessageBox) {
         "use strict";
 
         return Controller.extend("com.pe.proyectoIntegrador.controller.MainView", {
